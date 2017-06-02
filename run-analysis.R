6aa2ef690977ad818701b82ac95f008226068214
Marek Takac <marek.takac@nejremeslnici.cz>
2017-06-02 11:08:27 +0200

    Add run analysis script

---
+++ run-analysis.R
@@ -0,0 +1,11 @@
+## This script prepares data collected from the accelerometers from the Samsung Galaxy
+## S smartphone. For full description of the data see
+## http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+##
+## Goals:
+##    1. Merge the training and test sets to create one data set
+##    2. Extract only the measurments on the mean and standard deviation
+##    3. Use descriptive activity names
+##    4. Use descriptive variable names
+##    5. Create new, independent tidy data set with average of each variable for each
+##      activity and subject
